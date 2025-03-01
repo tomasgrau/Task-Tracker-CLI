c342f857fa9a2bec53ce1758aa37b360498f8e08
Tomas Grau <tomasgrau1301@gmail.com>
2025-03-01 01:16:17 -0300

    added some testing

--- Test/test_task_tracker.py
+++ Test/test_task_tracker.py
@@ -1,6 +1,13 @@
 import unittest
 import json
 import os
+
+import filecmp
+import sys
+from datetime import datetime
+
+sys.path.append("..")
+
 import task_tracker
 
 class TestCreateTaskFile(unittest.TestCase):
@@ -27,19 +34,150 @@ class TestCreateTaskFile(unittest.TestCase):
         self.assertTrue(os.path.exists("tasks.json"))
 
 class TestGetTasks(unittest.TestCase):
+    def setUp(self):
+        os.mkdir("test_dir")
+        os.chdir("test_dir")
+        with open("tasks.json", "w") as tasks:
+            json.dump(
+                [
+                    {
+                        "id": 0,
+                        "description": "Cocinar",
+                        "status": "to-do",
+                        "createdAt": "28/02/2025 17:35:07",
+                        "updatedAt": ""
+                    },
+                    {
+                        "id": 1,
+                        "description": "Cocinar 2",
+                        "status": "to-do",
+                        "createdAt": "01/03/2025 00:13:46",
+                        "updatedAt": ""
+                    }
+                ]
+            , tasks, indent=2)
+
+    def tearDown(self):
+        os.chdir("..")
+        for file in os.listdir("test_dir"):
+            os.remove(os.path.join("test_dir", file))
+        os.rmdir("test_dir")
+
     def test_get_tasks(self):
-        pass
+        self.assertEqual(task_tracker.get_tasks(),
+                         [
+                    {
+                        "id": 0,
+                        "description": "Cocinar",
+                        "status": "to-do",
+                        "createdAt": "28/02/2025 17:35:07",
+                        "updatedAt": ""
+                    },
+                    {
+                        "id": 1,
+                        "description": "Cocinar 2",
+                        "status": "to-do",
+                        "createdAt": "01/03/2025 00:13:46",
+                        "updatedAt": ""
+                    }
+                ]
+                )
 
 class TestWriteTask(unittest.TestCase):
+
+    def setUp(self):
+        os.mkdir("test_dir")
+        os.chdir("test_dir")
+        with open("test.json", "w") as tasks:
+            json.dump(
+                [
+                    {
+                        "id": 0,
+                        "description": "Cocinar",
+                        "status": "to-do",
+                        "createdAt": "28/02/2025 17:35:07",
+                        "updatedAt": ""
+                    },
+                    {
+                        "id": 1,
+                        "description": "Cocinar 2",
+                        "status": "to-do",
+                        "createdAt": "01/03/2025 00:13:46",
+                        "updatedAt": ""
+                    }
+                ]
+            , tasks, indent=2)
+            
+    def tearDown(self):
+        os.chdir("..")
+        for file in os.listdir("test_dir"):
+            os.remove(os.path.join("test_dir", file))
+        os.rmdir("test_dir")
+
     def test_write_task(self):
-        pass
+        task_tracker.write_tasks([
+                    {
+                        "id": 0,
+                        "description": "Cocinar",
+                        "status": "to-do",
+                        "createdAt": "28/02/2025 17:35:07",
+                        "updatedAt": ""
+                    },
+                    {
+                        "id": 1,
+                        "description": "Cocinar 2",
+                        "status": "to-do",
+                        "createdAt": "01/03/2025 00:13:46",
+                        "updatedAt": ""
+                    }
+                ])
+        self.assertTrue(filecmp.cmp("tasks.json", "test.json"))
+        
 
 class TestTaskTracker(unittest.TestCase):
+    def setUp(self):
+        os.mkdir("test_dir")
+        os.chdir("test_dir")
+        with open("tasks.json", "w") as task_file:
+            json.dump([],task_file)
+    
+    def tearDown(self):
+        os.chdir("..")
+        for file in os.listdir("test_dir"):
+            os.remove(os.path.join("test_dir", file))
+        os.rmdir("test_dir")
+
+
     def test_add(self):
-        pass 
+        new_task = {
+        "id": 0,
+        "description": "Cook",
+        "status": "to-do",
+        "createdAt": datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
+        "updatedAt": ""
+    }
+        task_tracker.add_task("Cook")
+        with open("tasks.json", "r") as tasks_file:
+            tasks = json.load(tasks_file)
+        self.assertEqual(new_task,tasks[0])
 
     def test_update(self):
-        pass
+        updated_task = {
+        "id": 0,
+        "description": "Cook 2",
+        "status": "to-do",
+        "createdAt": datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
+        "updatedAt": datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    }
+        task_tracker.add_task("Cook")
+        task_tracker.update_task(0, "Cook 2")
+        with open("tasks.json", "r") as tasks_file:
+            tasks = json.load(tasks_file)
+        self.assertEqual(updated_task, tasks[0])
+
+    def test_update_non_existing_task(self):
+        task_tracker.add_task("Cook")
+        self.assertEqual(task_tracker.update_task(12, "Cook"), False)
 
     def test_delete(self):
         pass
